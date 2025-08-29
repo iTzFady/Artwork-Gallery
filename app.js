e3103a4aca05fe80513aebb403a9adc156eaf32f
Faaady <47241432+iTzFady@users.noreply.github.com>
2025-08-29 22:47:12 +0300

    Updated wrong folder name

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use("/assets/imgs", express.static(path.join(__dirname, "assets", "imgs")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
